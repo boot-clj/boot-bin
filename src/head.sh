c207f0ed59523ec7a419f88161d055f17f139624
Paulus Esterhazy <pesterhazy@gmail.com>
2018-01-09 19:41:11 +0100

    Read boot.properties in boot script

--- src/head.sh
+++ src/head.sh
@@ -1,6 +1,41 @@
 #!/usr/bin/env bash
-declare -a "options=($BOOT_JVM_OPTIONS)"
+
+set -euo pipefail
+
+process_properties() {
+    if [[ -f "$1" ]]; then
+        while IFS='=' read -r key value; do
+            if [[ -n "$value" ]]; then
+                if [[ "$key" == "BOOT_JAVA_COMMAND" ]]; then
+                    java_command="$value"
+                elif [[ "$key" == "BOOT_JVM_OPTIONS" ]]; then
+                    options=($value)
+                fi
+            fi
+        done < "$1"
+    fi
+}
+
+# defaults
+
+java_command=java
+declare -a options=(${BOOT_JVM_OPTIONS-})
+
+# process properties files
+
+process_properties "${BOOT_HOME:-$HOME/.boot}/boot.properties"
+process_properties "boot.properties"
+
+# environment variables take precdence
+
+if [[ -n "${BOOT_JAVA_COMMAND-}" ]]; then
+    java_command="$BOOT_JAVA_COMMAND"
+fi
+if [[ -n "${BOOT_JVM_OPTIONS-}" ]]; then
+    options=($BOOT_JVM_OPTIONS)
+fi
+
 self="${BASH_SOURCE[0]}"
 selfdir="$(cd "$(dirname "${self}")" ; pwd)"
 selfpath="$selfdir/$(basename "$self")"
-exec ${BOOT_JAVA_COMMAND:-java} "${options[@]}" -Dboot.app.path="$selfpath" -jar "$0" "$@"
+exec "$java_command" "${options[@]}" -Dboot.app.path="$selfpath" -jar "$0" "$@"
